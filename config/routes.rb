f4cc9171322a50833ec56f936fe007f5f6579b71
juwanpetty <jchpetty@gmail.com>
2018-04-25 17:54:25 -0400

    Change routes to to appear more user friendly

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
+  
   get 'welcome/index'
   root 'welcome#index'
 
   get 'about', to: 'welcome#about'
+
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'registrations#new'
+  end
 
   resources :posts
 end
